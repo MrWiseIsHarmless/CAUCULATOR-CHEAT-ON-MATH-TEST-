7168745c07803e8c55a88a815f3e27bf025e787b
Mr.Wise <111756395+MrWiseIsHarmless@users.noreply.github.com>
2023-04-16 20:44:31 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,2 @@
+javascript:expr=prompt('Formula...(eg:  #*#  or # / # or # + # or # - #)','');if(expr!=null){with(Math){evl=parseFloat(eval(expr))};if(isNaN(evl)){alert('Really are you kidding me? Enter in a number, not that stuff!')}else{alert(evl)}}else{void(null)}
+
